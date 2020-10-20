b5d98508a73535296f336236091620ce74cd6df6
guchunjie (RD) <gu.chunjie@h3c.com>
2020-10-20 17:33:26 +0800

    init: init project

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/makeloffve/go-utils
+
+go 1.15
+
+require (
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+)
---
+++ log.go
@@ -0,0 +1,88 @@
+package go_utils
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"time"
+	"github.com/fsnotify/fsnotify"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	)
+
+func newRotateLogHook(logDir, baseFilename, logLevel string, rotateDays, maxRemainCnt uint) (logrus.Hook, error)  {
+	writer, err := rotatelogs.New(
+		path.Join(logDir, baseFilename+"-%Y-%m-%d.log"),
+		rotatelogs.WithLinkName(path.Join(logDir, baseFilename+".log")),
+		rotatelogs.WithRotationTime(time.Duration(rotateDays * 24) * time.Hour),
+		rotatelogs.WithRotationCount(maxRemainCnt),
+		)
+	if err != nil {
+		logrus.Errorf("config local file system for logger error: %v", err)
+		return nil, err
+	}
+
+	level, ok := logrus.ParseLevel(logLevel)
+
+	if ok == nil {
+		logrus.SetLevel(level)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	hook := lfshook.NewHook(lfshook.WriterMap{
+		logrus.DebugLevel: writer,
+		logrus.WarnLevel: writer,
+		logrus.InfoLevel: writer,
+		logrus.ErrorLevel: writer,
+		logrus.FatalLevel: writer,
+		logrus.PanicLevel: writer,
+	}, &logrus.TextFormatter{DisableColors:true})
+
+	return hook, nil
+}
+
+func loadConfig() *viper.Viper {
+	logConfViper := viper.New()
+	dir, _ := os.Getwd()
+	configFilepath := path.Join(dir, "conf", "log.yml")
+	logConfViper.SetConfigFile(configFilepath)
+	err := logConfViper.ReadInConfig()
+	if err != nil {
+		logrus.Fatalf("load config file: ../conf/log.yml error: %v", err)
+	}
+
+	logConfViper.WatchConfig()
+	logConfViper.OnConfigChange(func(event fsnotify.Event) {
+		logrus.Infof("config file: ../conf/log.yml has been changed: %s", event.Name)
+	})
+
+	return logConfViper
+}
+
+func InitLog()  {
+	logConf := loadConfig()
+
+	logConf.SetDefault("lograotate.baseFilename", "../conf/log.yml")
+	logConf.SetDefault("lograotate.logDir", "logs")
+	logConf.SetDefault("lograotate.logLevel", "info")
+	logConf.SetDefault("lograotate.maxRemainCnt", 30)
+	logConf.SetDefault("lograotate.rotateDays", 1)
+
+	logDir := logConf.GetString("lograotate.logDir")
+	baseFilename := logConf.GetString("lograotate.baseFilename")
+	logLevel := logConf.GetString("lograotate.logLevel")
+	maxRemainCnt := logConf.GetUint("lograotate.maxRemainCnt")
+	rotateDays := logConf.GetUint("lograotate.rotateDays")
+
+	if logHook, err := newRotateLogHook(logDir, baseFilename, logLevel, rotateDays, maxRemainCnt); err != nil {
+		panic(fmt.Errorf("newRotateLogHook error: %v.\n", err))
+	} else {
+		logrus.AddHook(logHook)
+	}
+
+}
+
+
